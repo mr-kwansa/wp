4af354eca793351ba1230b92e66fc1a1db9c4bb3
Mechanic <dumashieanthony@gmail.com>
2024-03-26 23:39:53 +0000

    made a temporary fix to create part. admin can now create an item.

--- product.php
+++ product.php
@@ -9,61 +9,59 @@ ini_set ('display_startup_errors', 'on');
 ini_set ('error_reporting', E_ALL);
 
 
-$errors = array ();
+// $errors = array ();
 
-//create post
+// //create post
 
-if(isset($_POST['createProduct'])){
-    //recieve all input from the create form
+// if(isset($_POST['createProduct'])){
+//     //recieve all input from the create form
 
 
-    require "connection.php";
+//     require "connection.php";
 
-    $product_id = $_POST['product_id'];
-    $product_name = $_POST['product_name'];
-    $product_info = $_POST['product_info'];
-    $item_price = $_POST['item_price'];
-    $product_photo = $_FILES['product_photo']['name'];
+//     $product_id = $_POST['product_id'];
+//     $product_name = $_POST['product_name'];
+//     $product_info = $_POST['product_info'];
+//     $item_price = $_POST['item_price'];
+//     $product_photo = $_FILES['product_photo']['name'];
 
 
     
 
 
-    //form validation: ensure that the form is properly filled
-    if(empty($product_id)){ array_push($errors,'Please give your product a unique ID');}
-    if(empty($product_name)){ array_push($errors,'Please name your Product');}
-    if(empty($item_price)){ array_push($errors,'Hi! How much is your item? Enter the price and try again');}
-    if(empty($product_photo)){ array_push($errors,'Select a photo of your item.');}
+//     //form validation: ensure that the form is properly filled
+//     if(empty($product_id)){ array_push($errors,'Please give your product a unique ID');}
+//     if(empty($product_name)){ array_push($errors,'Please name your Product');}
+//     if(empty($item_price)){ array_push($errors,'Hi! How much is your item? Enter the price and try again');}
+//     if(empty($product_photo)){ array_push($errors,'Select a photo of your item.');}
 
-    //process item photo
-    if($product_photo){
-        //upload photo to folder
-        require_once ('../utils/photo_manager.php');
-    }
-
-
-    if(empty($errors)){
-        $new_photo = $newFileName;
+//     //process item photo
+//     if($product_photo){
+//         //upload photo to folder
+//         require_once ('../utils/photo_manager.php');
+//     }
 
 
-        //create product
-        $stmt = mysqli_prepare($conn,"INSERT INTO products (product_id, product_name, product_photo, item_price, product_info)VALUES (?,?,?,?,?)");
-        
-        mysqli_stmt_bind_param($stmt,"sssss", $product_id, $product_name, $new_photo, $item_price, $product_info);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
+//     if(empty($errors)){
+//         $new_photo = $newFileName;
 
 
-        echo '<div class="alert alert-success" role="alert">'."Post added successfully".'</div>'; 
+//         //create product
+//         $stmt = mysqli_prepare($conn,"INSERT INTO products (product_id, product_name, product_photo, item_price, product_info)VALUES (?,?,?,?,?)");
 
-    }
-    if ($errors){
-        foreach ($errors as $error){
-            echo "<p class='text-danger text-center m-1'><strong>$error</strong></p>";
-        }
-    }
+//         mysqli_stmt_bind_param($stmt,"sssss", $product_id, $product_name, $new_photo, $item_price, $product_info);
+//         mysqli_stmt_execute($stmt);
+//         mysqli_stmt_close($stmt);
 
 
+//         echo '<div class="alert alert-success" role="alert">'."Post added successfully".'</div>'; 
+
+//     }
+//     if ($errors){
+//         foreach ($errors as $error){
+//             echo "<p class='text-danger text-center m-1'><strong>$error</strong></p>";
+//         }
+//     }
 
 
 
@@ -83,7 +81,3 @@ if(isset($_POST['createProduct'])){
 
 
 
-
-
-
-}
